4d11308b4dd6816fe422357ce12017317596a3aa
KimonoKurtRussell <n_hardin@hotmail.com>
2018-05-31 06:21:10 +0000

    added removing fields and css additions.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,7 @@
 $('document').ready(function() {
 
+//Jquery to compile options into an object to export to database
+
 
 let object = {};
   $('[name=Option1], [name=Option2]').on('keyup', function(e) {
@@ -12,28 +14,26 @@ let object = {};
 
 // on submit, grab object, stringify it and set val of hidden field with it
 
+
+
+//adding JQUERY to increase the amount of options a user can have per decision
 var max_fields = 7;
-    var wrapper = $(".new");
-    var add_button = $(".btn-lg");
+    var wrapper = $(".empty");
+    var add_button = $(".btn-md");
 
     var x = 2;
     $(add_button).click(function(e){
         e.preventDefault();
         if(x < max_fields){
             x++;
-            $(wrapper).append('<div><label for="Option">Option:</label><input type="text" class="form-control" name="Option2"></div>');
+            $(wrapper).append('<div><label for="Option">Option:</label><input type="text" class="form-control" name="Option2"><button class="remove_field">Remove</button></div>');
         }
     });
 
-    // $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
-    //     e.preventDefault(); $(this).parent('div').remove(); x--;
-    // })
-
+    $(wrapper).on("click",".remove_field", function(e){
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
 
 
 
 });
-
-
-
-
