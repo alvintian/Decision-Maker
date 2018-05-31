55b1973aa4c5ca555370f1c2b59d4a1070656155
alvintian <alvintian2003@yahoo.ca>
2018-05-31 18:29:10 +0000

    changed to frontpage form to all tables

--- routes/users.js
+++ routes/users.js
@@ -22,9 +22,8 @@ module.exports = (knex) => {
 			email: req.body.email
 		};
 		let insertpoll = {
-			poll_question: req.body.name,
-			email: req.body.email
-			user_id:
+			poll_question: req.body.question,
+			//			user_id: req:b
 		};
 		let insertoption = {
 			choice_description: req.body.op1
@@ -33,21 +32,53 @@ module.exports = (knex) => {
 			choice_description: req.body.op2
 		};
 
-		console.log(req.body, "users.js");
+		// console.log(req.body, "users.js");
 		// if (!req.body.text) {
 		// 	res.status(400).json({
 		// 		error: 'invalid request: no data in POST body'
 		// 	});
 		// 	return;
 		// }
+
 		knex
-			.insert(insertuser).into('users')
-			.insert(insertpoll).into('poll').select()
-			.insert(insertoption).into('option')
-			.insert(insertoption2).into('option')
-			.then((results) => {
-				res.json(results);
-			});
+			.insert(insertuser).into('users').returning('id')
+			//	.('poll').insert('req.body.question')
+			.then(response =>
+				knex.insert({
+					poll_question: req.body.question,
+					user_id_fk: response[0]
+				}).into('poll').returning('poll_id'))
+
+		//		.insert(insertpoll).into('poll').select('req.body.question', id).from('users')
+
+		.then(function(response) {
+				return knex('option')
+					.insert([{
+						choice_description: req.body.op1,
+						poll_id_fk: response[0]
+					}, {
+						choice_description: req.body.op2,
+						poll_id_fk: response[0]
+					}])
+			})
+			// .then(function(response) {
+			// 	return knex('option')
+			// 		.insert({
+			// 			choice_description: req.body.op2,
+			// 			poll_id: response[0]
+			// 		})
+			// })
+
+		//			from("pollToSelectDataFrom")).into("users")
+
+		// .insert(insertpoll).into('poll').select('req.body.question', id).from('users')
+		// 	.where('name').equals(req.body.name);
+		// .insert(insertoption).into('option')
+		// .insert(insertoption2).into('option')
+
+		.then((results) => {
+			res.json(results);
+		});
 	});
 	return router;
 }
