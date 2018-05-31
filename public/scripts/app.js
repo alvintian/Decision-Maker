5b9f7b6ec67b914fafa74bf8f9add412761acf83
KimonoKurtRussell <n_hardin@hotmail.com>
2018-05-31 04:11:34 +0000

    added addding text fields to home page, and part of the jquery for coalating optionf for shipment to db as object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,37 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+$('document').ready(function() {
+
+
+let object = {};
+  $('[name=Option1], [name=Option2]').on('keyup', function(e) {
+      object[$(e.target)[0].name] = $(e.target).val();
+
+    });
+
+
+
+
+// on submit, grab object, stringify it and set val of hidden field with it
+
+var max_fields = 7;
+    var wrapper = $(".new");
+    var add_button = $(".btn-lg");
+
+    var x = 2;
+    $(add_button).click(function(e){
+        e.preventDefault();
+        if(x < max_fields){
+            x++;
+            $(wrapper).append('<div><label for="Option">Option:</label><input type="text" class="form-control" name="Option2"></div>');
+        }
+    });
+
+    // $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+    //     e.preventDefault(); $(this).parent('div').remove(); x--;
+    // })
+
+
+
+
 });
 
 
