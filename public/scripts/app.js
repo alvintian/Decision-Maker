2cd899b262fd4337e72c903908162ac6feace815
KimonoKurtRussell <n_hardin@hotmail.com>
2018-05-31 18:56:35 +0000

    adding reults page, css addit. and js for doughnut

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -60,3 +60,28 @@ var max_fields = 7;
 	// 		},
 	// 	});
 	// });
+
+//adding Js to implement a doughnut graph needs to able to take data from database
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    datasets: [{
+      backgroundColor: [
+        "#2ecc71",
+        "#3498db",
+        "#95a5a6",
+        "#9b59b6",
+        "#f1c40f",
+        "#e74c3c",
+        "#34495e"
+      ],
+      data: [12, 19, 3, 17, 28, 24, 7]
+    }]
+  }
+});
+
+
+});
