db987d4cb1bc3de94b7ffb94147820bb28b69f3a
alvintian <alvintian2003@yahoo.ca>
2018-05-31 15:10:54 +0000

    more stuff

--- routes/users.js
+++ routes/users.js
@@ -17,11 +17,23 @@ module.exports = (knex) => {
 
 
 	router.post('/', (req, res) => {
-	var insert1 = {
-		name: req.body.name,
-		email: req.body.email
-	};
-	console.log(req.body,"users.js");
+		let insertuser = {
+			name: req.body.name,
+			email: req.body.email
+		};
+		let insertpoll = {
+			poll_question: req.body.name,
+			email: req.body.email
+			user_id:
+		};
+		let insertoption = {
+			choice_description: req.body.op1
+		};
+		let insertoption2 = {
+			choice_description: req.body.op2
+		};
+
+		console.log(req.body, "users.js");
 		// if (!req.body.text) {
 		// 	res.status(400).json({
 		// 		error: 'invalid request: no data in POST body'
@@ -29,7 +41,10 @@ module.exports = (knex) => {
 		// 	return;
 		// }
 		knex
-			.insert(insert1).into('users')
+			.insert(insertuser).into('users')
+			.insert(insertpoll).into('poll').select()
+			.insert(insertoption).into('option')
+			.insert(insertoption2).into('option')
 			.then((results) => {
 				res.json(results);
 			});
