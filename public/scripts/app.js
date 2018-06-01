45c88011ae76093b8efc4394832f65d5f56d3b71
KimonoKurtRussell <n_hardin@hotmail.com>
2018-06-01 00:29:09 +0000

    fixing stylesheets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,47 +16,39 @@ $( "#button" ).click(function() {
 
 
 
-	$.ajax({
-		method: "GET",
-		url: "/api/users"
-	}).done((users) => {
-		for (user of users) {
-			$("<div>").text(user.name).appendTo($("body"));
-		}
-	});
 
 
-	//Array()
-	$(".tester").on("click", function() {
-	let submission = $("form[class='info_input']").serialize();
-	$("#results").text(submission);
-		console.log(submission, "test");
-			$.ajax({
-		url: '/api/users',
-		method: 'POST',
-		data: {
-			submission
-		}
-		//		body: JSON.stringify($(this).serialize())
-	});
-		// jQuery.each(submission, function(i, field) {
-		// 	$("#results").append(field.value + " ");
-		// });
+  //Array()
+  $(".tester").on("click", function() {
+  let submission = $("form[class='info_input']").serialize();
+  $("#results").text(submission);
+    console.log(submission, "test");
+      $.ajax({
+    url: '/api/users',
+    method: 'POST',
+    data: {
+      submission
+    }
+    //    body: JSON.stringify($(this).serialize())
+  });
+    // jQuery.each(submission, function(i, field) {
+    //  $("#results").append(field.value + " ");
+    // });
 
 
-	});
+  });
 
-	// $submission.on('click', function() {
-	// 	tweet.count = parseInt(tweet.count) + 1;
-	// 	$.ajax({
-	// 		url: '/tweets/likes',
-	// 		method: 'POST',
-	// 		data: {
-	// 			id: tweet._id,
-	// 			count: tweet.count
-	// 		},
-	// 	});
-	// });
+  // $submission.on('click', function() {
+  //  tweet.count = parseInt(tweet.count) + 1;
+  //  $.ajax({
+  //    url: '/tweets/likes',
+  //    method: 'POST',
+  //    data: {
+  //      id: tweet._id,
+  //      count: tweet.count
+  //    },
+  //  });
+  // });
 
 //adding Js to implement a doughnut graph needs to able to take data from database
 
