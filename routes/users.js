501d968c3c6f62ec1b2813e8bf4724c338fb1bc5
Chantal <chandesrochers@gmail.com>
2018-06-02 20:47:19 +0000

    added some of Alvin's files

--- routes/users.js
+++ routes/users.js
@@ -77,33 +77,50 @@ module.exports = (knex) => {
           poll_id_fk: response[0]
         })))
     })
-
-    // [{
-    //         choice_description: req.body.op1,
-    //         poll_id_fk: response[0]
-    //       }, {
-    //         choice_description: req.body.op2,
-    //         poll_id_fk: response[0]
-    //       }]
-    // .then(function(response) {
-    //  return knex('option')
-    //    .insert({
-    //      choice_description: req.body.op2,
-    //      poll_id: response[0]
-    //    })
-    // })
-
-    //      from("pollToSelectDataFrom")).into("users")
-
-    // .insert(insertpoll).into('poll').select('req.body.question', id).from('users')
-    //  .where('name').equals(req.body.name);
-    // .insert(insertoption).into('option')
-    // .insert(insertoption2).into('option')
-
     .then((results) => {
-      res.redirect(`/polls/thankyou/${pollurlID}`);
-
+         res.redirect(`/polls/thankyou/${pollurlID}`);
+        });
     });
-  });
+
+   router.post('/result', (req, res) => {
+        let updatechoice = [];
+        let updatescore = [];
+        let matchX = [];
+        let superX = [];
+        for (let x in req.body) {
+          if (String(x) !== "submit_button") {
+            updatechoice.push({
+              choice_description: x
+            })
+            updatescore.push({
+              score: req.body[x]
+            })
+            matchX.push(x);
+            superX.push(req.body[x]);
+          }
+        }
+        let loopyloop = [];
+        for (let i = 0; i < updatescore.length; i++) {
+          let loopy = knex('option').increment('score', parseInt(superX[i])).
+          where({poll_id_fk: 3,choice_description: matchX[i]});
+          loopyloop.push(loopy);
+        }
+        //            for (let x in req.body) {
+        //             if (String(x) !== "submit_button") {
+        //       console.log(req.body[x], x);
+        // knex.raw(update option set score = case(choice_description when superX[i])
+        //   updatescore where updatechoice)
+        //      knex.raw(update option set score = score + superX[i] where )
+        //       .update(updatescore[0])
+        console.log(loopyloop.map(function(e) {
+            return
+            e.toString();
+          }));
+          Promise.all(loopyloop).then(response => {
+            res.json(response);
+          })
+        })
+
+
   return router;
 }
--- server.js
+++ server.js
@@ -48,25 +48,6 @@ const deletePoll = require("./public/scripts/deletePoll")(knex);
 const queryOptions = require("./public/scripts/queryOptions")(knex);
 const findPollConf = require("./public/scripts/findPollConf")(knex);
 
-// var sendEmail = require('gmail-send')({
-// //var send = require('../index.js')({
-//   user: 'manydecisions@gmail.com',
-//   // user: credentials.user,                  // Your GMail account used to send emails
-//   pass: 'hcrp kpco nbyt xphz',
-//   // pass: credentials.pass,                  // Application-specific password
-//   to:   'chandesrochers@gmail.com',
-//   // to:   credentials.user,                  // Send to yourself
-//                                            // you also may set array of recipients:
-//                                            // [ 'user1@gmail.com', 'user2@gmail.com' ]
-//   // from:    credentials.user,            // from: by default equals to user
-//   // replyTo: credentials.user,            // replyTo: by default undefined
-//   // bcc: 'some-user@mail.com',            // almost any option of `nodemailer` will be passed to it
-//   subject: 'test subject',
-//   text:    'gmail-send example 1',         // Plain text
-//   //html:    '<b>html text</b>'            // HTML
-// });
-
-
 
 app.get("/", (req, res) => {
   res.render("index");
